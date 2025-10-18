e2807b4413f4c61b94c5367c8520e934e565f5f1
Gabriel Silva <gabbrielssilva.biel@gmail.com>
2025-10-17 22:25:25 -0400

    solução 2 questão B ABC 426

---
+++ Contests/ABC426/B2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for(auto &nx: s){ mp[nx]++; }
+    for(auto &nx: mp){
+        if(nx.second==1){
+            cout << nx.first << endl;
+        }
+    }
+    return 0;
+}
